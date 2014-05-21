1c919c3cdfb3eef64b9770a714a3030394aa52a2
Nimish Gupta <nimish@couchbase.com>
2014-05-21 12:24:53 +0530

    first commit

---
+++ i18n/states/IN.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Indian states
+ *
+ * @author 		WooThemes
+ * @category 	i18n
+ * @package 	WooCommerce/i18n
+ * @version     2.0.0
+ */
+global $states;
+
+$states['IN'] = array(
+	'KA' => __( 'Karnataka', 'woocommerce' ),
+);
